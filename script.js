69497337af13f3dab9d8ef76ccf3004ba9450125
Mario Vinandy <134607100+mariovinandyy@users.noreply.github.com>
2023-10-24 18:42:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed =  new Typed(".greetings", {
+    strings:["Halo!", "您好!", "Bonjour!", "こんにちは!", "안녕하세요!","Здравствуйте!", "नमस्ते!", "Olá!", "السلام عليكم!", "Hello!"],
+    typeSpeed:100,
+    backSpeed:60,
+    backDelay: 5000,
+    startDelay: 1000,
+    loop:true
+})
---
+++ script1.js
@@ -0,0 +1,9 @@
+var typed =  new Typed(".aboutWhat", {
+    strings:["Me", "My Skills", "My Experiences", "My Education"],
+    showCursor: false,
+    typeSpeed:50,
+    backSpeed:60,
+    backDelay: 5000,
+    startDelay: 1000,
+    loop:true
+})
